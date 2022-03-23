3c37da7cf13115b1f23fc3f1a4e03923a4736a9e
yigitkaraduman <yigitkaraduman@gmail.com>
2022-03-23 16:16:35 +0300

    exam timer is added.

--- public/js/editor.js
+++ public/js/editor.js
@@ -133,6 +133,35 @@ const detectRestrictions = function (userCode) {
     editorLib.printToConsole('log log--error');
 }
 
+var examTimer = function(date){
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        var countDownDate = new Date(date).getTime();
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("remaining").innerHTML = hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("remaining").innerHTML = "EXPIRED";
+        }
+    }, 1000)
+}
+
+
 // Events
 executeCodeBtn.addEventListener('click', () => {
     // Clear console messages
