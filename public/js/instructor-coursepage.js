b23c95eb8ddb7e40722c2b842427829a709186a9
yigitkaraduman <yigitkaraduman@gmail.com>
2022-04-14 15:33:15 +0300

    create exam time & name update done

--- public/js/instructor-coursepage.js
+++ public/js/instructor-coursepage.js
@@ -1,7 +1,19 @@
-const addExam = function(name, lecture_id){
-    let data = {name: name, lecture_id: lecture_id};
+const addExam = function(name, start, end,lecture_id){
+    let data = {name: name, start:start, end:end, lecture_id: lecture_id};
 
     fetch("/instructor-main/create-exam/", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(data)
+    }).then(res => {
+        
+    });
+}
+
+const saveExamSettings = function(name, start, end, lecture_id, exam_id){
+    let data = {name: name, start:start, end:end, lecture_id: lecture_id, exam_id: exam_id};
+
+    fetch("/instructor-main/edit-exam/", {
     method: "POST",
     headers: {'Content-Type': 'application/json'}, 
     body: JSON.stringify(data)
--- routes/instructormain.js
+++ routes/instructormain.js
@@ -40,23 +40,36 @@ router.post('/lecture', isLoggedIn, async (req, res) => {
 
 //create new exam
 router.post('/create-exam', async(req,res) =>{
-    const {name, lecture_id} = req.body;
+    const {name, start, end, lecture_id} = req.body;
 
     const newExamObj = {
         lesson_id: lecture_id,
         name:name,
-        start_time:'',
-        end_time:'',
+        start_time:start,
+        end_time:end,
         configs:{restrictedFuncs:[], restrictedLibs:[], language:'java'},
         questions:['//---question1 ---']
     };
+
     let newExam = new Exam(newExamObj);
 
     await newExam.save();
   
 })
 
+//edit exam settings
+router.post('/edit-exam', async(req,res) =>{
+    const {name, start, end, lecture_id, exam_id} = req.body;
 
+    const exam = await Exam.findById(exam_id);
+    exam.name = name;
+    exam.start_time = start;
+    exam.end_time = end;
+        
+    await exam.save();
+})
+
+//set restrictions for the exam
 router.post('/:examid/set-restriction', async(req,res) =>{
     const {examid} = req.params;
     const{type, restricted} = req.body;
@@ -73,9 +86,10 @@ router.post('/:examid/set-restriction', async(req,res) =>{
 
     var escapedExam = setObj(exam);
     
-    res.render('create-exam-editor', {exam:escapedExam,exam_id: exam._id,restrictedFuncs: exam.configs.restrictedFuncs, restrictedLibs: exam.configs.restrictedLibs, qnum:''});
+    res.render('create-exam-editor', {exam:escapedExam,exam_id: exam._id, start: exam.start_time, end: exam.end_time, name: exam.name, restrictedFuncs: exam.configs.restrictedFuncs, restrictedLibs: exam.configs.restrictedLibs, qnum:''});
 })
 
+//set language for the exam
 router.post('/:examid/set-language', async(req,res) =>{
     const {examid} = req.params;
     const{language} = req.body;
@@ -87,10 +101,10 @@ router.post('/:examid/set-language', async(req,res) =>{
 
     var escapedExam = setObj(exam);
 
-    //res.render('create-exam-editor', {exam:escapedExam, restrictedFuncs: exam.configs.restrictedFuncs, restrictedLibs: exam.configs.restrictedLibs, language: exam.configs.language});
-    
 })
 
+
+//add questions for the exam
 router.post('/:examid/add-question', async(req,res) =>{
     const {examid} = req.params;
     const exam = await Exam.findById(examid);
@@ -104,17 +118,19 @@ router.post('/:examid/add-question', async(req,res) =>{
     
 });
 
+//see selected question
 router.get('/exam/ans/:id/:q', async (req, res) => {
     const { id, q} = req.params;
     const exam = await Exam.findById(id);
     
     var escapedExam = setObj(exam);
 
-    res.render('create-exam-editor', { exam: escapedExam, exam_id: exam._id,restrictedFuncs: exam.configs.restrictedFuncs, restrictedLibs: exam.configs.restrictedLibs, 
+    res.render('create-exam-editor', { exam: escapedExam, exam_id: exam._id, start: exam.start_time, end: exam.end_time, name: exam.name, restrictedFuncs: exam.configs.restrictedFuncs, restrictedLibs: exam.configs.restrictedLibs, 
         language: exam.configs.language, qnum: q});
 
 })
 
+//save current question content
 router.post('/exam/:id/:q', async (req, res) =>{
     const { id, q} = req.params;
     const { content } = req.body;
